3d0ee8eb1efc3d5d652d2ba5bd19901c1ddb6838
SGeriDEV <sarffygeri@gmail.com>
2021-05-21 16:26:34 +0200

    Https redirect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HttpsRedirect from "react-https-redirect";
+
 import App from "./App";
 
 import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById("root")
+);
